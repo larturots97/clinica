b655df8ddb5fde0c6ad5ace65b97ea4442acd836
Luis Arturo <larturots97@gmail.com>
2026-03-15 16:43:38 -0700

    registrar middlewares de Spatie

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'medico' => \App\Http\Middleware\MedicoMiddleware::class,
-        ]);
-    })
+    $middleware->alias([
+        'medico'     => \App\Http\Middleware\MedicoMiddleware::class,
+        'role'       => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+    ]);
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
